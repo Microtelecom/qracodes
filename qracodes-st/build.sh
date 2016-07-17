4129fe342b4d4c35effd37db4205d09df2c5e44e
iv3nwv <nicopal@microtelecom.it>
2016-07-17 20:16:50 +0200

    Added APSET features/SNR estimation

--- qracodes-st/build.sh
+++
@@ -1,3 +0,0 @@
-g++ -march=native -O3 -DFTZ_ENABLE -DQRA_DEBUG -DTEST_WER_AWGN     *.c -o qracodesawgn      && ./qracodesawgn
-g++ -march=native -O3 -DFTZ_ENABLE -DQRA_DEBUG -DTEST_WER_RAYLEIGH *.c -o qracodesrayleigh  && ./qracodesrayleigh
-
